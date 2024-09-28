cd886507917f70ff9b2b3378b06cec0cb0d31bd9
Elijah Mondero <elijahmondero@gmail.com>
2024-09-29 12:37:54 +1300

    resolve warnings

---
+++ elijahmondero/vite.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0]; // Create chunks by package
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjusted limit
+  },
+});
